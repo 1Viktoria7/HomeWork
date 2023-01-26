1a3e7a9313e4fb2fab1b160875293ae8e56c5522
1Viktoria7 <vika-chibisova@mail.ru>
2023-01-27 00:26:41 +0300

    Sync

--- HomeWorkProject/src/main/java/org/example/Main.java
+++ HomeWorkProject/src/main/java/org/example/Main.java
@@ -2,6 +2,22 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pet pet = new Pet("Питомец", 1);
+        Cat cat = new Cat("Киса", 3);
+        Dog dog = new Dog("Микки",4);
+
+        System.out.println("Class Pet");
+        pet.makeNoise();
+        pet.eatSomething();
+
+        System.out.println("\nClass Cat");
+        cat.drinkMilk();
+        cat.makeNoise();
+        cat.eatSomething();
+
+        System.out.println("\nClass Dog");
+        dog.buryBone();
+        dog.makeNoise();
+        dog.eatSomething();
     }
 }
