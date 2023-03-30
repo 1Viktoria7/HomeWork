c287d57bf321e936d0e9d5859cd087b56fd8983a
1Viktoria7 <vika-chibisova@mail.ru>
2023-03-30 22:24:03 +0300

    sync

--- HomeWorkProject/src/main/java/org/example/HumiditySensor.java
+++ HomeWorkProject/src/main/java/org/example/HumiditySensor.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class HumiditySensor {
-     double max = 100.0;
+    private double max = 100.0;
     public double getHumidity() {
         double randomHumidity = Math.random() * max;
         return randomHumidity;
