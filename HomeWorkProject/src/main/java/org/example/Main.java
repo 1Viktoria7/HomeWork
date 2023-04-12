ced09d8f8e384053b4cfa62dd206561d63b2fcc8
1Viktoria7 <vika-chibisova@mail.ru>
2023-04-12 22:54:48 +0300

    Sync

--- HomeWorkProject/src/main/java/org/example/Main.java
+++ HomeWorkProject/src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import okhttp3.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.sql.SQLOutput;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    static String baseUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+    public static void getOrderByOrderId(String cityName) throws IOException {
+        HttpUrl.Builder urlBuilder = HttpUrl.get(baseUrl).newBuilder();
+        urlBuilder.addQueryParameter("q", cityName);
+
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Call call = client.newCall(request);
+        Response response = call.execute();
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        getOrderByOrderId("10");
     }
 }
