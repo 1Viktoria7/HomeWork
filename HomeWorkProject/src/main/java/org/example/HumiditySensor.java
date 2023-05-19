4dd4949538abf3b4957a07a351d45ce853e258ff
1Viktoria7 <122374154+1Viktoria7@users.noreply.github.com>
2023-05-19 17:27:34 +0300

    Revert "Промежуточная аттестация, задания 1 и 2."

--- HomeWorkProject/src/main/java/org/example/HumiditySensor.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class HumiditySensor {
-     double max = 100.0;
-    public double getHumidity() {
-        double randomHumidity = Math.random() * max;
-        return randomHumidity;
-    }
-}
